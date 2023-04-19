96d292ea98212caf36ab774d5f18abce403de77d
Arun Vijay <arunvjx1@gmail.com>
2023-04-19 13:38:46 +0100

    This code was not in running condition. Due to an error.I Have fixed the error.

--- src/main/java/com/arun/studentportal/MiscellaneousBeans.java
+++ src/main/java/com/arun/studentportal/MiscellaneousBeans.java
@@ -14,6 +14,7 @@ import java.util.Locale;
 @Configuration
 class MiscellaneousBeans {
 
+
     @Bean
     public LocaleResolver localeResolver() {
         SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
